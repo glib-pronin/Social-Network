84ae2c059b6b88183badfece8c911827ecda51c6
glib-pronin <0953044497gleb@gmail.com>
2026-03-07 15:05:31 +0200

    Change send code, add logs

--- user_app/static/user_app/js/sendCode.js
+++ user_app/static/user_app/js/sendCode.js
@@ -5,12 +5,14 @@ function showError(msg, show=false) {
 }
 
 async function sendCode() {
-    console.log('...');
+    console.log('sending');
     const res = await fetch('/send-code', {
         method: 'POST',
         headers: {"Content-Type": "application/json", "X-CSRFToken": document.getElementById('auth-form').elements.csrfmiddlewaretoken.value},
         body: JSON.stringify()
     })
+    console.log('sent');
+    console.log(res)1;
     const { success } = await res.json()
     if (!success) {
         showError('Сталася помилка під час відправки коду, спробуйте пізніше!', true)
