961ad501df624a3e3468278e2ce37c48fb50e180
glib-pronin <0953044497gleb@gmail.com>
2026-03-07 14:47:36 +0200

    Add STATICFILES_STORAGE

--- SocialNetwork/settings.py
+++ SocialNetwork/settings.py
@@ -165,3 +165,5 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
